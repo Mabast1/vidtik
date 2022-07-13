52121b39554a2bb3f2036784932f39bf6cca3d4c
Mabast <mabast.niga@yahoo.com>
2022-07-13 14:07:50 -0500

    implemented video play and pause

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -17,13 +17,51 @@ interface IProps {
 
 const Detail = ({ postDetails }: IProps) => {
   const [post, setPost] = useState(postDetails);
+  const [playing, setPlaying] = useState(false);
 
-  const videoRef = useRef(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const onVideoClick = () => {
+    if (playing) {
+      videoRef?.current?.pause();
+      setPlaying(false);
+    } else {
+      videoRef?.current?.play();
+      setPlaying(true);
+    }
+  };
 
   if (!post) return null;
 
   return (
-    <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap"></div>
+    <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
+      <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-gray-300">
+        <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
+          <p>
+            <MdOutlineCancel className="text-white text-[35px]" />
+          </p>
+        </div>
+        <div className="relative">
+          <div className="lg:h-[100vh] h-[60vh]">
+            <video
+              src={post.video.asset.url}
+              ref={videoRef}
+              onClick={onVideoClick}
+              className="h-full cursor-pointer"
+            ></video>
+          </div>
+          <div className="absolute top-[45%] left-[45%] cursor-pointer">
+            {!playing && (
+              <div>
+                <button type="button" onClick={onVideoClick} className="">
+                  <BsFillPlayFill className="text-white shadow-md text-6xl lg:text-8xl" />
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
