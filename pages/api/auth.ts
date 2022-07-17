83294a4dd79d468af12369d9b5c01b694bebd2c0
Mabast <mabast.niga@yahoo.com>
2022-07-16 22:55:50 -0500

    fetch all users function completion and api routing

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -6,9 +6,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const doc = req.body;
+  if (req.method === "POST") {
+    const doc = req.body;
 
-  client.createIfNotExists(doc).then(() => {
-    res.status(200).json("Login successful");
-  });
+    client
+      .createIfNotExists(doc)
+      .then(() => res.status(200).json("Login successful"));
+  }
 }
