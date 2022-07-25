d27d6f941aa7fd306194408691c8565ce444f965
Mabast <mabast.niga@yahoo.com>
2022-07-24 23:45:41 -0500

    created dynamic page and api request for search

---
+++ pages/search/[searchTerm].tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import { BsCheckCircleFill } from "react-icons/bs";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import VideoCard from "../../components/VideoCard";
+import NoResults from "../../components/NoResults";
+import { IUser, Video } from "../../types";
+import { BASE_URL } from "../../utils";
+import useAuthStore from "../../store/authStore";
+
+const Search = () => {
+  return (
+    <div className="">
+      <div></div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async ({
+  params: { searchTerm },
+}: {
+  params: { searchTerm: string };
+}) => {
+  const res = await axios.get(`${BASE_URL}/api/search/${searchTerm}`);
+
+  return {
+    props: { data: res.data },
+  };
+};
+
+export default Search;
