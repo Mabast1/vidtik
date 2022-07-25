fd80627d4112820a36440e6f65d83db8152b1480
Mabast <mabast.niga@yahoo.com>
2022-07-24 23:54:44 -0500

    created layout for search page results

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -12,9 +12,33 @@ import { BASE_URL } from "../../utils";
 import useAuthStore from "../../store/authStore";
 
 const Search = ({ videos }: { videos: Video[] }) => {
+  const [isAccounts, setIsAccounts] = useState(false);
+
+  const accounts = isAccounts ? "border-b-2 border-black" : "text-gray-400";
+  const isVideos = !isAccounts ? "border-b-2 border-black" : "text-gray-400";
+
   return (
-    <div className="">
-      <div></div>
+    <div className="w-full">
+      <div className="">
+        <div className="flex gap-10 mb-10 mt-10 border-b border-gray-200 bg-white">
+          <div className={`w-1/4 flex justify-center ${accounts}`}>
+            <p
+              className="text-xl font-semibold cursor-pointer mt-2"
+              onClick={() => setIsAccounts(true)}
+            >
+              Accounts
+            </p>
+          </div>
+          <div className={`w-1/4 flex justify-center ${videos}`}>
+            <p
+              className="text-xl font-semibold cursor-pointer mt-2"
+              onClick={() => setIsAccounts(false)}
+            >
+              Videos
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
