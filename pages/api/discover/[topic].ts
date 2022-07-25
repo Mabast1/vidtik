0c071e5c72cc888d3ff95712d63c1f8896b5f997
Mabast <mabast.niga@yahoo.com>
2022-07-25 00:59:51 -0500

    fixed typo for getting posts

--- pages/api/discover/[topic].ts
+++ pages/api/discover/[topic].ts
@@ -7,7 +7,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
+  if (req.method === "GET") {
     const { topic } = req.query;
 
     const videosQuery = topicPostsQuery(topic);
