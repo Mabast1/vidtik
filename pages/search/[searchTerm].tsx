35be975f09b0589106dcb326ddbc57de0295ff6c
Mabast <mabast.niga@yahoo.com>
2022-07-25 00:44:43 -0500

    completed search mapping for accounts

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -45,7 +45,36 @@ const Search = ({ videos }: { videos: Video[] }) => {
         </div>
       </div>
       {isAccounts ? (
-        <div className="mdmt-16">{}</div>
+        <div className="md:mt-16">
+          {searchedAccounts.length > 0 ? (
+            searchedAccounts.map((user: IUser, idx: number) => (
+              <Link href={`/profile/${user._id}`} key={idx}>
+                <div className="flex items-start gap-3 p-2 cursor-pointer font-semibold rounded border-b border-gray-200">
+                  <div>
+                    <Image
+                      src={user.image}
+                      alt="user profile"
+                      width={50}
+                      height={50}
+                      className="rounded-full"
+                    />
+                  </div>
+                  <div className="hidden xl:block ">
+                    <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+                      {user.userName.replaceAll(" ", "")}
+                      <BsCheckCircleFill className="text-blue-400" />
+                    </p>
+                    <p className="capitalize text-gray-400 text-xs">
+                      {user.userName}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <NoResults text={`No accounts were found for ${searchTerm}`} />
+          )}
+        </div>
       ) : (
         <div className="md:mt-16 flex flex-wrap gap-6 md:justify-start">
           {videos.length ? (
@@ -53,7 +82,7 @@ const Search = ({ videos }: { videos: Video[] }) => {
               <VideoCard post={video} key={idx} />
             ))
           ) : (
-            <NoResults text={`No results for ${searchTerm}`} />
+            <NoResults text={`No videos were found for ${searchTerm}`} />
           )}
         </div>
       )}
